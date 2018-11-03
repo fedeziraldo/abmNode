86df79379e5eeb720188b415c3bcc9951bfbe48a
Rafa <rafazira83@gmail.com>
2018-11-03 19:22:01 -0300

    asd

--- abmMongo.js
+++ abmMongo.js
@@ -1,19 +1,27 @@
 var app = require('express')();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
+var bodyParser = require('body-parser');
+var path    = require("path");
 
-MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("ABMNode");
-    dbo.collection("productos").find({}).toArray(function (err, result) {
+app.post('/consulta', function (req, res) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
-        console.log(result);
-        db.close();
+        var dbo = db.db("ABMNode");
+        dbo.collection("productos").find({}).toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+            res.send(result)
+        });
     });
-});
-
+})
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
-    res.send("Hello puto!");
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
-
+app.post('/form',function(req,res){
+    console.log(req.body.email)
+})
 app.listen(3000);
