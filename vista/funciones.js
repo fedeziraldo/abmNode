e73104b29fc83f78cfdb6c980d7a0b6756ade859
Diego <ziraldodiego@gmail.com>
2018-11-10 17:16:40 -0300

    front

--- vista/funciones.js
+++ vista/funciones.js
@@ -1,4 +1,17 @@
 function mostrar(){
-  document.getElementById("botonArt").style.opacity = "1"
-
+  document.getElementById("botonArt").style.opacity = "1";
 }
+function apagar(){
+  document.getElementById("botonArt").style.opacity = "0.5";
+}
+
+function mostrar_ocultar(){
+  var boton = document.getElementById("botonArt");
+
+    if(botonArt.style.opacity == "0.5"){
+      mostrar();
+    }
+    else{
+      apagar();
+    }
+} 
