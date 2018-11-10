01ca9db0db2060631adcf0f7f80a94ec66ea145f
fedeziraldo <federrico04@gmail.com>
2018-11-10 17:15:48 -0300

    merge

--- controlador.js
+++ controlador.js
@@ -4,10 +4,6 @@ var path= require("path");
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 8d22da2bbe9540cca50f109c7e59f5a85bbc15f9
 var producto=require("./modelo/producto.js");
 var nuevoProducto=new producto();
 nuevoProducto.setprecio(500);
@@ -39,19 +35,6 @@ rutas.get('/carga',function(req,res){
       });
     })
 
-
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("ABMNode");
-    var myobj = { precio: req.producto.precio, descripcion: req.producto.descripcion };
-    dbo.collection("productos").insertOne(myobj, function(err, res) {
-      if (err) throw err;
-      console.log("exitoso");
-      db.close();
-    });
-  });
-})
 rutas.get('/borrarProducto', function (req, res) {
 
     MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
