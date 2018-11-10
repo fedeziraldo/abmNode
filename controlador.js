0e164042e737b6b17da76f724c743772275173c8
92909 <federrico04@gmail.com>
2018-11-10 16:52:53 -0300

    merge

--- controlador.js
+++ controlador.js
@@ -3,17 +3,13 @@ var rutas = app.Router();
 var path= require("path");
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-<<<<<<< HEAD
 
-rutas.post('/consulta', function (req, res) {
-=======
 var producto=require("./modelo/producto.js");
 var nuevoProducto=new producto();
 nuevoProducto.setprecio(500);
 console.log(nuevoProducto.getprecio())
 nuevoProducto.setdescripcion('buzo');
 rutas.get('/consulta', function (req, res) {
->>>>>>> 43bc18bb163e0c3c0fd20d8249f0934849136181
     MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
         var dbo = db.db("ABMNode");
@@ -26,26 +22,20 @@ rutas.get('/consulta', function (req, res) {
     });
 })
 rutas.get('/carga',function(req,res){
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("ABMNode");
+        var myobj = { precio: req.producto.precio, descripcion: req.producto.descripcion };
+        dbo.collection("productos").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("exitoso");
+          db.close();
+        });
+      });
+    })
 
 
-<<<<<<< HEAD
 rutas.post('/borrarProducto', function (req, res) {
-=======
-<<<<<<< HEAD
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("ABMNode");
-    var myobj = { precio: req.producto.precio, descripcion: req.producto.descripcion };
-    dbo.collection("productos").insertOne(myobj, function(err, res) {
-      if (err) throw err;
-      console.log("exitoso");
-      db.close();
-    });
-  });
-})
-=======
-rutas.get('/borrarProducto', function (req, res) {
->>>>>>> 43bc18bb163e0c3c0fd20d8249f0934849136181
     MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
         if (err) throw err;
         var dbo = db.db("ABMNode");
@@ -57,7 +47,6 @@ rutas.get('/borrarProducto', function (req, res) {
     });
 })
 
->>>>>>> 10856567060558744d4d980c301fc7a916dbc90d
 rutas.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/vista/index.html'));
 });
